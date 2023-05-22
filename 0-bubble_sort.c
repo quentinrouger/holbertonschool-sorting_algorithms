46a8cb038757ae537db4019f29cfb7b467fe0c69
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-05-22 11:44:16 +0200

    add file: main.h sort.h 0-bubble_sort.c print_array.c and print_list.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Trie un tableau d'entiers en utilisant l'algorithme Bubble Sort.
+ *
+ * @array: Le tableau à trier.
+ * @size: La taille du tableau.
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+
+    for (i = 0; i < size - 1; i++) {
+        for (j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                // Échange des éléments
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+
+                // Affichage du tableau après l'échange
+                for (size_t k = 0; k < size; k++) {
+                    printf("%d ", array[k]);
+                }
+                printf("\n");
+            }
+        }
+    }
+}
---
+++ sort.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define  _MAIN_H_
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+void bubble_sort(int *array, size_t size);
+
+#endif 
