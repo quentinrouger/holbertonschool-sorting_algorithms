c73fb73925d38eb152c1effd3aeac8bdce9381ce
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-05-22 13:56:51 +0200

    modification files: 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,45 +2,42 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	if (*list == NULL || (*list)->next == NULL)
-		return;
+    if (*list == NULL || (*list)->next == NULL)
+        return;
 
-	listint_t *sorted;
-	listint_t *current;
+    listint_t *sorted = *list;
+    listint_t *current = sorted->next;
 
-	sorted = *list;
-	current = sorted->next;
+    while (current != NULL)
+    {
+        listint_t *insertion_point = sorted;
+        listint_t *next = current->next;
 
-	while (current != NULL)
-	{
-		listint_t *insertion_point = sorted;
-		listint_t *next = current->next;
+        while (insertion_point != NULL && insertion_point->n > current->n)
+        {
+            insertion_point = insertion_point->prev;
+        }
 
-		while (insertion_point != NULL && insertion_point->n > current->n)
-		{
-			insertion_point = insertion_point->prev;
-		}
+        if (insertion_point == NULL)
+        {
+            current->prev = NULL;
+            current->next = sorted;
+            sorted->prev = current;
+            sorted = current;
+        }
+        else
+        {
+            current->prev = insertion_point;
+            current->next = insertion_point->next;
 
-		if (insertion_point == NULL)
-		{
-			current->prev = NULL;
-			current->next = sorted;
-			sorted->prev = current;
-			sorted = current;
-		}
-		else
-		{
-			current->prev = insertion_point;
-			current->next = insertion_point->next;
+            if (insertion_point->next != NULL)
+                insertion_point->next->prev = current;
 
-			if (insertion_point->next != NULL)
-				insertion_point->next->prev = current;
+            insertion_point->next = current;
+        }
 
-			insertion_point->next = current;
-		}
+        current = next;
+    }
 
-		current = next;
-	}
-
-	*list = sorted;
+    *list = sorted;
 }
