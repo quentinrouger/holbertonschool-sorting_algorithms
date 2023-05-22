b5a4af352e78b4acf417b6207cb81c9c5bc48ec9
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-05-22 13:22:46 +0200

    modification file: 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,45 +2,45 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	if (*list == NULL || (*list)->next == NULL)
-		return;
+    listint_t *sorted = NULL;  /* Pointeur vers la position triée actuelle */
+    listint_t *current = *list;  /* Pointeur vers le nœud actuel */
 
-	listint_t *sorted = NULL;  // Pointeur vers la position triée actuelle
-	listint_t *current = *list;  // Pointeur vers le nœud actuel
+    if (*list == NULL || (*list)->next == NULL)
+        return;
 
-	while (current != NULL)
-	{
-		listint_t *next = current->next;  // Pointeur vers le prochain nœud
+    while (current != NULL)
+    {
+        listint_t *next = current->next;  /* Pointeur vers le prochain nœud */
 
-		while (sorted != NULL && sorted->n > current->n)
-		{
-			sorted = sorted->prev;  // Déplace le pointeur vers la gauche
-		}
+        while (sorted != NULL && sorted->n > current->n)
+        {
+            sorted = sorted->prev;  /* Déplace le pointeur vers la gauche */
+        }
 
-		if (sorted == NULL)
-		{
-			// Insère le nœud au début de la liste
-			current->prev = NULL;
-			current->next = *list;
-			(*list)->prev = current;
-			*list = current;
-		}
-		else
-		{
-			// Insère le nœud à la position triée appropriée
-			current->prev = sorted;
-			current->next = sorted->next;
+        if (sorted == NULL)
+        {
+            /* Insère le nœud au début de la liste */
+            current->prev = NULL;
+            current->next = *list;
+            (*list)->prev = current;
+            *list = current;
+        }
+        else
+        {
+            /* Insère le nœud à la position triée appropriée */
+            current->prev = sorted;
+            current->next = sorted->next;
 
-			if (sorted->next != NULL)
-				sorted->next->prev = current;
+            if (sorted->next != NULL)
+                sorted->next->prev = current;
 
-			sorted->next = current;
-		}
+            sorted->next = current;
+        }
 
-		sorted = current;  // Met à jour le pointeur vers la position triée actuelle
-		current = next;  // Passe au prochain nœud
+        sorted = current;  /* Met à jour le pointeur vers la position triée actuelle */
+        current = next;  /* Passe au prochain nœud */
 
-		// Affichage de la liste après l'échange
-		print_list(*list);
-	}
+        /* Affichage de la liste après l'échange */
+        print_list(*list);
+    }
 }
