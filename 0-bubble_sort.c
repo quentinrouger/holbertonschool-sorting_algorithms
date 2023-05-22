ab14c9b552ad808895368f0ce72dc7dcd23a478c
quentinrouger <quentin_rouger@hotmail.fr>
2023-05-22 04:01:26 -0700

    Add: the function print_array in 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,26 +8,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j, k;
+    size_t i, j;
     int temp;
 
-    for (i = 0; i < size - 1; i++) 
+    for (i = 0; i < size - 1; i++)
     {
-        for (j = 0; j < size - i - 1; j++) 
+        for (j = 0; j < size - i - 1; j++)
         {
-            if (array[j] > array[j + 1]) 
+            if (array[j] > array[j + 1])
             {
                 /* Échange des éléments */
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
-
-                /* Affichage du tableau après l'échange */
-                for (k = 0; k < size; k++) 
-                {
-                    printf("%d ", array[k]);
-                }
-                printf("\n");
+		print_array(array, size);
             }
         }
     }
