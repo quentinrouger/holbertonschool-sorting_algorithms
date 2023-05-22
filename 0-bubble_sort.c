958a24cd756c83fa43788b275e4605190e59f055
quentinrouger <quentin_rouger@hotmail.fr>
2023-05-22 04:05:19 -0700

    Add: the function print_array to 0_bubble_sort.c file and fix betty style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,28 +1,29 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Trie un tableau d'entiers en utilisant l'algorithme Bubble Sort.
+ * bubble_sort - Trie un tableau d'entiers en utilisant
+ * l'algorithme Bubble Sort.
  *
  * @array: Le tableau à trier.
  * @size: La taille du tableau.
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
+	size_t i, j;
+	int temp;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                /* Échange des éléments */
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-		print_array(array, size);
-            }
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/* Échange des éléments */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
