0d536a633fb742aa4cb007232e995284456c4df5
quentinrouger <quentin_rouger@hotmail.fr>
2023-05-23 02:22:08 -0700

    Fix: Change the code of insertion_sort_list function and modify 1-O

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,28 +1,39 @@
 #include "sort.h"
 
+/**
+ * insertion_sort_list - function that sorts a doubly linked list
+ * of integers in ascending order
+ * @array: The array
+ * @size: Number of elements in the array
+ *
+*/
 void insertion_sort_list(listint_t **list)
 {
-    listint_t **sorted;
-    listint_t *current;
-    listint_t **insertion_point;
+	listint_t *newNode;
 
-    if (*list == NULL || (*list)->next == NULL)
-        return;
+	if (*list == NULL || (*list)->next == NULL)
+		return;
 
-    sorted = list;
-    insertion_point = sorted;
-    while ((*sorted)->next && (*sorted)->next != NULL)
-    {
-        (*sorted) = (*sorted)->next;
-    }
-    while ((*sorted)->prev && (*sorted)->prev == NULL)
-    {
-        current = (*sorted)->prev;
-        if ((current->n) > ((*sorted)->n))/*sorted doit etre toujours superieur a courent*/
-        {
-            (*insertion_point)->prev = current;
-        }     
-        (*sorted) = (*sorted)->prev;
-    }
-    list = insertion_point;
+	newNode = (*list)->next;
+
+	while (newNode != NULL)
+	{
+		while (newNode->prev && newNode->n < newNode->prev->n)
+		{
+			newNode->prev->next = newNode->next;
+			if (newNode->next)
+				newNode->next->prev = newNode->prev;
+			newNode->next = newNode->prev;
+			newNode->prev = newNode->prev->prev;
+			if (newNode->prev)
+				newNode->prev->next = newNode;
+
+			newNode->next->prev = newNode;
+
+			if (!newNode->prev)
+				*list = newNode;
+			print_list(*list);
+		}
+		newNode = newNode->next;
+	}
 }
