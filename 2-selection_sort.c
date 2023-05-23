b2e9c39598c3740d229eca113b4b076fd4614783
quentinrouger <quentin_rouger@hotmail.fr>
2023-05-23 03:47:06 -0700

    Fix: betty style on 2-selection_sort_list.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,30 +2,32 @@
 
 /**
  * selection_sort - Trie un tableau d'entiers par ordre croissant en utilisant
- *                  l'algorithme de tri par selection.
+ * l'algorithme de tri par selection.
  * @array: Le tableau d'entiers a trier.
  * @size: La taille du tableau.
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, init, temp, j;
-    if (array == NULL)
-            return;
-    for (i = 0; i < size - 1; i++)
-    {
-        init = i;
+	size_t i, init, temp, j;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[init])
-                init = j;
-        }
-        if (array[i] != array[init])
-        {
-            temp = array[i];
-            array[i] = array[init];
-            array[init] = temp;
-            print_array(array, size);
-        }
-    }
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		init = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[init])
+				init = j;
+		}
+		if (array[i] != array[init])
+		{
+			temp = array[i];
+			array[i] = array[init];
+			array[init] = temp;
+			print_array(array, size);
+		}
+	}
 }
