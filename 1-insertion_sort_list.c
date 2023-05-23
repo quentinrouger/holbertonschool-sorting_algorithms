e1a05b67cef77c613fcc0da0c35304b1166d3b36
quentinrouger <quentin_rouger@hotmail.fr>
2023-05-23 12:55:34 -0700

    Add: a NULL condition

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,7 +10,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *newNode;
 
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	newNode = (*list)->next;
